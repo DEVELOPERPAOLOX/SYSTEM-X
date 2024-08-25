8c571a8d74e04e171c5ee144b0a0efa12d9e0f0b
olagente38 <benjaminmijahuanga@gmail.com>
2024-08-25 04:01:20 -1100

    Update play-systemx.js

--- plugins/play-systemx.js
+++ plugins/play-systemx.js
@@ -1,4 +1,4 @@
-// C O D I G O   C R E A D O   P O R   P A O L O   X
+// Ｃ Ｏ Ｄ Ｉ Ｇ Ｏ   Ａ Ｄ Ａ Ｐ Ｔ Ａ Ｄ Ｏ   Ｐ Ｏ Ｒ   Ｄ Ｅ Ｖ Ｅ Ｌ Ｏ Ｐ Ｅ Ｒ   Ｐ Ａ Ｏ Ｌ Ｏ   Ｘ
 import fg from 'api-dylux'
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 import yts from 'yt-search'
