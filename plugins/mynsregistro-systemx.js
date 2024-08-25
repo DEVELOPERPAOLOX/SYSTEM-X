b495b6b74a35cbb78b91107efa644cb788834974
olagente38 <benjaminmijahuanga@gmail.com>
2024-08-25 03:59:22 -1100

    Update and rename rpg-myns.js to mynsregistro-systemx.js

--- plugins/mynsregistro-systemx.js
+++ plugins/mynsregistro-systemx.js
@@ -6,7 +6,7 @@ const handleRequest = async function (message, { conn, text, usedPrefix }) {
   const seriesNumber = createHash('md5').update(message.sender).digest('hex');
 
   // Enviar una respuesta falsa con el número de serie generado
-  conn.fakeReply(message.chat, seriesNumber, '0@s.whatsapp.net', '乂 N U M E R O - D E - S E R I E 乂', 'status@broadcast');
+  conn.fakeReply(message.chat, seriesNumber, '0@s.whatsapp.net', 'ＮＵＭＥＲＯ ＤＥ ＳＥＲＩＥ | ＳＹＳＴＥＭ Ｘ', 'status@broadcast');
 }
 
 // Configuración del comando
