84e100ed2879ec9974f6cc525a7c96a12659e5d7
olagente38 <benjaminmijahuanga@gmail.com>
2024-08-25 17:29:16 -1100

    Update stickerttp-x.js

--- plugins/stickerttp-x.js
+++ plugins/stickerttp-x.js
@@ -6,12 +6,12 @@ let text
         text = args.slice(0).join(" ");
     } else if (m.quoted && m.quoted.text) {
         text = m.quoted.text;
-    } else throw "*[❗️] Uso incorrecto del comando, agregue un texto*";
-   if (!text) return m.reply('*[❗️] Uso incorrecto del comando, agregue un texto*');
+    } else throw "👋🏻𝐒𝐚𝐥𝐮𝐝𝐨𝐬 𝐮𝐬𝐮𝐚𝐫𝐢𝐨!\n🚀𝐕𝐮𝐞𝐥𝐯𝐞 𝐚 𝐮𝐬𝐚𝐫 𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐩𝐞𝐫𝐨 𝐜𝐨𝐧 𝐞𝐥 𝐮𝐬𝐨 𝐜𝐨𝐫𝐫𝐞𝐜𝐭𝐨𝐬𝐨.\n✅𝐄𝐣𝐞𝐦𝐩𝐥𝐨: .𝐭𝐭𝐩 + 𝐭𝐞𝐱𝐭𝐨";
+   if (!text) return m.reply('👋🏻𝐒𝐚𝐥𝐮𝐝𝐨𝐬 𝐮𝐬𝐮𝐚𝐫𝐢𝐨!\n🚀𝐕𝐮𝐞𝐥𝐯𝐞 𝐚 𝐮𝐬𝐚𝐫 𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐩𝐞𝐫𝐨 𝐜𝐨𝐧 𝐞𝐥 𝐮𝐬𝐨 𝐜𝐨𝐫𝐫𝐞𝐜𝐭𝐨𝐬𝐨.\n✅𝐄𝐣𝐞𝐦𝐩𝐥𝐨: .𝐭𝐭𝐩 + 𝐭𝐞𝐱𝐭𝐨');
     const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
     const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
     const mishi = text.replace(mentionRegex, '');
-   if (mishi.length > 30) return m.reply('*[❗️] El texto no puede tener mas de 30 caracteres*');
+   if (mishi.length > 30) return m.reply('❎𝐈𝐧𝐭𝐞𝐧𝐭𝐚 𝐝𝐞 𝐧𝐮𝐞𝐯𝐨. 𝐄𝐥 𝐭𝐞𝐱𝐭𝐨 𝐧𝐨 𝐩𝐮𝐞𝐝𝐞 𝐭𝐞𝐧𝐞𝐫 +𝟑𝟎 𝐜𝐚𝐫𝐚𝐜𝐭𝐞𝐫𝐞𝐬.');
     const pp = await conn.profilePictureUrl(who).catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
     const nombre = await conn.getName(who)
     const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || nombre}`, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
