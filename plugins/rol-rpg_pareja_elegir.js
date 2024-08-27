5e1a9b4184d7c6dfeb193dfe37d8cc5a3657fddb
olagente38 <benjaminmijahuanga@gmail.com>
2024-08-27 04:44:54 -1100

    Update rol-rpg_pareja_elegir.js

--- plugins/rol-rpg_pareja_elegir.js
+++ plugins/rol-rpg_pareja_elegir.js
@@ -1,4 +1,3 @@
-//import { areJidsSameUser } from '@adiwajshing/baileys'
 let { areJidsSameUser } = (await import(global.baileys)).default
 let toM = a => '@' + a.split('@')[0]
 let handler = async (m, { conn, usedPrefix, command, text, participants, groupMetadata}) => {
@@ -15,9 +14,8 @@ do b = ps.getRandom()
 while (b === a)
     
 if (command == 'futurarelacion') {    
-let caption = `💗 𝗙𝗨𝗧𝗨𝗥𝗔 𝗣𝗔𝗥𝗘𝗝𝗔 💗\n${toM(a)} 💞 ${toM(b)}\n\n💌 𝗠𝗘𝗡𝗦𝗔𝗝𝗘 𝗗𝗘 𝗔𝗠𝗢𝗥\n*_${await ktnmbk.getRandom()}_*`
+let caption = `💗𝐅𝐔𝐓𝐔𝐑𝐀 𝐏𝐀𝐑𝐄𝐉𝐀:\n${toM(a)} 💞 ${toM(b)}\n\n💌𝐌𝐒𝐉 𝐃𝐄 𝐀𝐌𝐎𝐑:\n*_${await ktnmbk.getRandom()}_*`
 await conn.reply(m.chat, caption, m, { mentions: conn.parseMention(caption) })
-//await conn.sendButton(m.chat, caption, `*_Si quiere tener una pareja use el comando ${usedPrefix}pareja etiquetando a tu futura Pareja._*\n\n` + wm, null, [['🤭 𝙌𝙐𝙀 𝙎𝙀 𝘿𝙀𝘾𝙇𝘼𝙍𝙀𝙉', `amor`],['🧐 𝙊𝙏𝙍𝘼 𝙋𝘼𝙍𝙀𝙅𝘼', `${usedPrefix}futurarelacion`],['😆 𝙀𝙎 𝙐𝙉𝘼 𝙋𝘼𝙍𝙀𝙅𝘼 𝙍𝘼𝙉𝘿𝙊𝙈', `😂`]], m, { mentions: conn.parseMention(caption) })
 }}
 	
 if(isNaN(text)) {
@@ -41,16 +39,12 @@ var user = number + '@s.whatsapp.net'
 let users = m.isGroup ? participants.find(v => areJidsSameUser(v.jid == user)) : {}
 	
 if(!users) return await conn.reply(m.chat, `${fg}𝙉𝙊 𝙎𝙀 𝙀𝙉𝘾𝙊𝙉𝙏𝙍𝙊 𝘼 𝙇𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼, 𝘿𝙀𝘽𝙀 𝘿𝙀 𝙀𝙎𝙏𝘼𝙍 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙂𝙍𝙐𝙋𝙊\n\n𝙏𝙃𝙀 𝙋𝙀𝙍𝙎𝙊𝙉 𝙒𝘼𝙎 𝙉𝙊𝙏 𝙁𝙊𝙐𝙉𝘿, 𝙏𝙃𝙀𝙔 𝙈𝙐𝙎𝙏 𝘽𝙀 𝙄𝙉 𝙏𝙃𝙄𝙎 𝙂𝙍𝙊𝙐𝙋`, fkontak,  m)
-//await conn.sendButton(m.chat, `${fg}𝙉𝙊 𝙎𝙀 𝙀𝙉𝘾𝙊𝙉𝙏𝙍𝙊 𝘼 𝙇𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼, 𝘿𝙀𝘽𝙀 𝘿𝙀 𝙀𝙎𝙏𝘼𝙍 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙂𝙍𝙐𝙋𝙊\n\n𝙏𝙃𝙀 𝙋𝙀𝙍𝙎𝙊𝙉 𝙒𝘼𝙎 𝙉𝙊𝙏 𝙁𝙊𝙐𝙉𝘿, 𝙏𝙃𝙀𝙔 𝙈𝙐𝙎𝙏 𝘽𝙀 𝙄𝙉 𝙏𝙃𝙄𝙎 𝙂𝙍𝙊𝙐𝙋`, wm, null, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], fkontak, m)
-	
+
 if(user === m.sender) return await conn.reply(m.chat, `${fg}𝙐𝙎𝙏𝙀𝘿 𝙈𝙄𝙎𝙈𝙊 𝙉𝙊 𝙋𝙐𝙀𝘿𝙀 𝙎𝙀𝙍 𝙋𝘼𝙍𝙀𝙅𝘼\n\n𝙔𝙊𝙐 𝙔𝙊𝙐𝙍𝙎𝙀𝙇𝙁 𝘾𝘼𝙉𝙉𝙊𝙏 𝘽𝙀 𝘼 𝙋𝘼𝙍𝙏𝙉𝙀𝙍`, fkontak,  m)
-//await conn.sendButton(m.chat, `${fg}𝙐𝙎𝙏𝙀𝘿 𝙈𝙄𝙎𝙈𝙊 𝙉𝙊 𝙋𝙐𝙀𝘿𝙀 𝙎𝙀𝙍 𝙋𝘼𝙍𝙀𝙅𝘼\n\n𝙔𝙊𝙐 𝙔𝙊𝙐𝙍𝙎𝙀𝙇𝙁 𝘾𝘼𝙉𝙉𝙊𝙏 𝘽𝙀 𝘼 𝙋𝘼𝙍𝙏𝙉𝙀𝙍`, wm, null, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], fkontak, m)
 	
 if(user === conn.user.jid) return await conn.reply(m.chat, `${fg}𝙔𝙊 𝙉𝙊 𝙋𝙐𝙀𝘿𝙊 𝙎𝙀𝙍 𝙎𝙐 𝙋𝘼𝙍𝙀𝙅𝘼 😹\n\n𝙒𝙄𝙏𝙃 𝙈𝙀 𝙔𝙊𝙐 𝘾𝘼𝙉𝙉𝙊𝙏 𝘽𝙀 𝘼 𝘾𝙊𝙐𝙋𝙇𝙀`, fkontak,  m)
-//await conn.sendButton(m.chat, `${fg}𝙔𝙊 𝙉𝙊 𝙋𝙐𝙀𝘿𝙊 𝙎𝙀𝙍 𝙎𝙐 𝙋𝘼𝙍𝙀𝙅𝘼 😹\n\n𝙒𝙄𝙏𝙃 𝙈𝙀 𝙔𝙊𝙐 𝘾𝘼𝙉𝙉𝙊𝙏 𝘽𝙀 𝘼 𝘾𝙊𝙐𝙋𝙇𝙀`, wm, null, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], fkontak, m)
 
 if (typeof global.db.data.users[user] == "undefined") return await conn.reply(m.chat, `${fg}𝙇𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼 𝙌𝙐𝙀 𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝙊 𝙉𝙊 𝙀𝙎𝙏𝘼 𝙀𝙉 𝙈𝙄 𝘽𝘼𝙎𝙀 𝘿𝙀 𝘿𝘼𝙏𝙊𝙎\n\n𝙏𝙃𝙀 𝙋𝙀𝙍𝙎𝙊𝙉 𝙄 𝙏𝘼𝙂 𝙄𝙎 𝙉𝙊𝙏 𝙄𝙉 𝙈𝙔 𝘿𝘼𝙏𝘼𝘽𝘼𝙎𝙀`, fkontak,  m)
-//await conn.sendButton(m.chat, `${fg}𝙇𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼 𝙌𝙐𝙀 𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝙊 𝙉𝙊 𝙀𝙎𝙏𝘼 𝙀𝙉 𝙈𝙄 𝘽𝘼𝙎𝙀 𝘿𝙀 𝘿𝘼𝙏𝙊𝙎\n\n𝙏𝙃𝙀 𝙋𝙀𝙍𝙎𝙊𝙉 𝙄 𝙏𝘼𝙂 𝙄𝙎 𝙉𝙊𝙏 𝙄𝙉 𝙈𝙔 𝘿𝘼𝙏𝘼𝘽𝘼𝙎𝙀`, wm, null, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], fkontak, m)
 	
 let pacar = global.db.data.users[user].pasangan
 let spac = global.db.data.users[m.sender].pasangan
@@ -77,8 +71,7 @@ conn.reply(m.chat, `Felicitaciones, oficialmente están saliendo @${user.split('
 }else {
 	
 global.db.data.users[m.sender].pasangan = user
-await conn.reply(m.chat, `*_${await ktnmbk.getRandom()}_*\n\n*@${toUser}* 𝙎𝙀 𝙀𝙎𝙏𝘼 𝘿𝙀𝘾𝙇𝘼𝙍𝘼𝙉𝘿𝙊!!! 😳\n𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 *@${who.split`@`[0]}* 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙀 𝘼 𝙇𝘼 𝘿𝙀𝘾𝙇𝘼𝙍𝘼𝘾𝙄𝙊𝙉 🙀\n\n❤️ *_Si quieres una Relacion escriba:_*\n*➠ ${usedPrefix}aceptar @${toUser}*\n\n💔 *_De no querer una Relacion escriba:_*\n*➠ ${usedPrefix}rechazar @${toUser}*\n\n${wm}`, m, { contextInfo: { mentionedJid: [ who, m.sender, user ]}})
-//await conn.sendButton(m.chat, `*_${await ktnmbk.getRandom()}_*\n\n*${await conn.getName(m.sender)}* 𝙎𝙀 𝙀𝙎𝙏𝘼 𝘿𝙀𝘾𝙇𝘼𝙍𝘼𝙉𝘿𝙊!!! 😳\n𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 *${await conn.getName(who)}* 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙀 𝘼 𝙇𝘼 𝘿𝙀𝘾𝙇𝘼𝙍𝘼𝘾𝙄𝙊𝙉 🙀`, `❤️ *_Si quieres una Relacion escriba:_*\n*➠ ${usedPrefix}aceptar @${conn.getName(m.sender)}*\n\n💔 *_De no querer una Relacion escriba:_*\n*➠ ${usedPrefix}rechazar @${conn.getName(m.sender)}*\n\n` + wm, null, [['🥳 𝙑𝘼𝙈𝙊𝙎 𝘼𝘾𝙀𝙋𝙏𝘼', `Amor`]], m, { contextInfo: { mentionedJid: [ who, m.sender, user ]}})
+await conn.reply(m.chat, `*_${await ktnmbk.getRandom()}_*\n\n\`🧸𝐃𝐄𝐂𝐋𝐀𝐑𝐀𝐃𝐎𝐑(𝐀):\` *@${toUser}*\n\`💗𝐃𝐄𝐒𝐓𝐈𝐍𝐀𝐓𝐀𝐑𝐈𝐎(𝐀):\` *@${who.split`@`[0]}*\n\n❤️𝐒𝐢 𝐝𝐞𝐬𝐞𝐚𝐬 𝐚𝐜𝐞𝐩𝐭𝐚𝐫 𝐞𝐬𝐜𝐫𝐢𝐛𝐞:\n*➠ ${usedPrefix}aceptar @${toUser}*\n\n💔𝐒𝐢 𝐝𝐞𝐬𝐞𝐚𝐬 𝐫𝐞𝐜𝐡𝐚𝐳𝐚𝐫 𝐞𝐬𝐜𝐫𝐢𝐛𝐞:\n*➠ ${usedPrefix}rechazar @${toUser}*`, m, fake, { contextInfo: { mentionedJid: [ who, m.sender, user ]}})
 }}}
 
 handler.command = /^(futurarelacion|pareja|elegirpareja|serpareja|sersupareja|couple|elegirpareja)$/i
@@ -87,43 +80,6 @@ handler.register = true
 export default handler
 
 let ktnmbk = [
-"Hay momentos en los que no me gusta estar solo. Pero tampoco quiero que todos me acompañen, solo te quiero a ti",
-"¡Me acabo de dar cuenta de que has sido rico todo este tiempo! Como lo que he estado buscando todo este tiempo. ¿Quieres ser mi novia?",
-"Agradezco a mis ojos, porque estos ojos me llevaron a encontrarte",
-"¿Puedo enviarte un CV o no? Porque quiero postularme para ser novia",
-"No soy el más grande, pero estoy seguro que si puedo hacerte feliz con amor y cariño, ¿me quieres verdad?",
-"Solo soy una persona común que tiene muchos defectos y puede que no merezca tu amor, pero si estás dispuesto a aceptarme como amante, prometo hacer lo que sea mejor para ti. ¿Aceptarás mi amor?",
-"Quiero decir algo. Me gusta desde hace mucho tiempo, pero no me atrevo a decirlo. Entonces, decidí solo WA. Quiero que seas mi novia",
-"Quiero decir algo que no puedo contener más. Te amo, ¿serás mi novia?",
-"Quiero ser una persona que pueda hacerte reír y sonreír todos los días. ¿Serás mi novia?",
-"Quiero tener una charla seria contigo. Todo este tiempo he albergado sentimientos por ti y siempre he estado pendiente de ti. Si no te importa, ¿quieres ser mi novia?",
-"Te miro y veo el resto de mi vida ante mis ojos.",
-"No tengo todo, pero al menos tengo suficiente amor para ti",
-"Me gustaste desde el principio. Eres tan simple, pero la sencillez es muy especial a mis ojos. Será perfecto si eres especial en mi corazón",
-"Realmente estoy enamorado de ti. ¿Serás mía?",
-"No te dije que no porque no tengo cupo ni crédito, pero estoy disfrutando de este anhelo por ti. Tal vez te sorprendas al escuchar eso. Siempre me has gustado",
-"No quiero que seas el sol de mi vida, porque aunque hace calor estás muy lejos. Tampoco quiero que seas aire, porque aunque te necesito y estás muy cerca, pero todos pueden respirarte también. Solo quiero que seas sangre que pueda estar muy cerca de mí",
-"No sé hasta cuándo terminará mi edad. Todo lo que sé es que mi amor es para siempre solo para ti",
-"Realmente disfruté el tiempo que pasamos juntos hoy. También nos conocemos desde hace mucho tiempo. En este día soleado, quiero expresarte que te amo",
-"Siempre imaginé lo hermoso que sería si algún día pudiéramos construir un arca de una casa y vivir juntos hasta el final de la vida. Sin embargo, todo eso no habría sucedido si los dos no hubiéramos estado juntos hasta ahora. ¿Serás mi amante?",
-"Me preparo mentalmente para hoy. Tienes que ser mi novia para tratar este amor incontrolable",
-"Sé que no tenemos la misma edad, pero ¿puedo vivir contigo por el resto de mi vida?",
-"Sé que hemos sido amigos durante mucho tiempo. ¿Pero no está mal si me gustas? Cualquiera que sea tu respuesta, acepto. Lo más importante es ser honesto desde el fondo de mi corazón",
-"No puedo empezar esto primero, pero te daré un código que me gustas. Si entiendes este código, estaremos juntos",
-"Soy demasiado estúpido o eres demasiado egoísta para hacer que me enamore de ti",
-"Cualquier cosa sobre ti, nunca he encontrado aburrimiento en ello. Porque estar a tu lado, el regalo más hermoso para mí. Sé mi amante, hey tú",
-"Con el permiso de Alá y la bendición de mamá papá, ¿quieres ser mi novio o no?",
-"¿Y si nos convertimos en una banda de ladrones? Yo robé tu corazón y tú me robaste el mío",
-"Feliz es que tú y yo nos hemos convertido en nosotros",
-"Mañana, si no funciona, puedo registrarme para ser tu novia. Déjame tener trabajo para pensar siempre en ti",
-"Déjame hacerte feliz para siempre. Solo tienes que hacer una cosa: Enamórate de mí",
-"Que toda mi alegría sea tuya, toda tu tristeza sea mía. ¡Que el mundo entero sea tuyo, solo tú seas mía!",
-"Que el pasado sea mi pasado, pero por el presente, ¿serás tú mi futuro?",
-"¿Puedes darme una dirección a tu corazón? Parece que me he perdido en tus ojos",
-"No es el trono o el tesoro lo que busco, sino el retorno de mi amor lo que espero de ti. La respuesta es sí",
-"La forma en que puedes hacerme reír incluso en los días más oscuros me hace sentir más ligero que cualquier otra cosa. Quiero que seas mía",
-"Mi amor por ti es incuestionable porque este amor es sincero desde el fondo de mi corazón",
-"Mi amor por ti es como un número del 5 al 10. No hay segundo. Quiero que seas la única mujer en mi corazón",
-"Qué chico se atreve a lastimarte. Aquí te trataré, mientras quieras ser mi novia",
-"Oye, ¿qué estás haciendo? Sal de la casa y mira la luna esta noche. La luz es hermosa y encantadora, pero sería aún más hermosa si yo estuviera a tu lado. ¿Qué tal si estamos juntos?"
+"\`💌𝐌𝐒𝐉 𝐃𝐄 𝐀𝐌𝐎𝐑:\`\n𝐃𝐞𝐬𝐝𝐞 𝐞𝐥 𝐩𝐫𝐢𝐦𝐞𝐫 𝐦𝐨𝐦𝐞𝐧𝐭𝐨 𝐪𝐮𝐞 𝐧𝐮𝐞𝐬𝐭𝐫𝐨𝐬 𝐜𝐚𝐦𝐢𝐧𝐨𝐬 𝐬𝐞 𝐜𝐫𝐮𝐳𝐚𝐫𝐨𝐧, 𝐬𝐞𝐧𝐭í 𝐪𝐮𝐞 𝐡𝐚𝐛í𝐚 𝐞𝐧𝐜𝐨𝐧𝐭𝐫𝐚𝐝𝐨 𝐚𝐥𝐠𝐨 𝐯𝐞𝐫𝐝𝐚𝐝𝐞𝐫𝐚𝐦𝐞𝐧𝐭𝐞 𝐞𝐬𝐩𝐞𝐜𝐢𝐚𝐥. 𝐓𝐮𝐬 𝐩𝐚𝐥𝐚𝐛𝐫𝐚𝐬, 𝐭𝐮 𝐫𝐢𝐬𝐚, 𝐜𝐚𝐝𝐚 𝐦𝐨𝐦𝐞𝐧𝐭𝐨 𝐜𝐨𝐦𝐩𝐚𝐫𝐭𝐢𝐝𝐨 𝐡𝐚 𝐡𝐞𝐜𝐡𝐨 𝐪𝐮𝐞 𝐦𝐢 𝐜𝐨𝐫𝐚𝐳ó𝐧 𝐥𝐚𝐭𝐢𝐞𝐫𝐚 𝐜𝐨𝐧 𝐮𝐧𝐚 𝐢𝐧𝐭𝐞𝐧𝐬𝐢𝐝𝐚𝐝 𝐪𝐮𝐞 𝐧𝐮𝐧𝐜𝐚 𝐡𝐚𝐛í𝐚 𝐜𝐨𝐧𝐨𝐜𝐢𝐝𝐨.\n\n𝐐𝐮𝐢𝐞𝐫𝐨 𝐪𝐮𝐞 𝐬𝐞𝐩𝐚𝐬 𝐪𝐮𝐞 𝐜𝐚𝐝𝐚 𝐝𝐢𝐚 𝐜𝐨𝐧𝐭𝐢𝐠𝐨 𝐞𝐬 𝐮𝐧 𝐫𝐞𝐠𝐚𝐥𝐨. 𝐌𝐢 𝐯𝐢𝐝𝐚 𝐡𝐚 𝐜𝐚𝐦𝐛𝐢𝐚𝐝𝐨 𝐩𝐚𝐫𝐚 𝐦𝐞𝐣𝐨𝐫 𝐝𝐞𝐬𝐝𝐞 𝐪𝐮𝐞 𝐭𝐞 𝐜𝐨𝐧𝐨𝐜𝐢, 𝐲 𝐦𝐞 𝐝𝐨𝐲 𝐜𝐮𝐞𝐧𝐭𝐚 𝐝𝐞 𝐪𝐮𝐞 𝐧𝐨 𝐩𝐮𝐞𝐝𝐨 𝐢𝐦𝐚𝐠𝐢𝐧𝐚𝐫 𝐮𝐧 𝐟𝐮𝐭𝐮𝐫𝐨 𝐬𝐢𝐧 𝐭𝐢 𝐚 𝐦𝐢 𝐥𝐚𝐝𝐨.\n\n𝐇𝐨𝐲, 𝐦𝐞 𝐚𝐭𝐫𝐞𝐯𝐨 𝐚 𝐝𝐚𝐫 𝐮𝐧 𝐩𝐚𝐬𝐨 𝐦á𝐬 𝐲 𝐝𝐞𝐜𝐥𝐚𝐫𝐚𝐫 𝐪𝐮𝐞 𝐦𝐢 𝐚𝐦𝐨𝐫 𝐩𝐨𝐫 𝐭𝐢 𝐜𝐫𝐞𝐜𝐞 𝐜𝐨𝐧 𝐜𝐚𝐝𝐚 𝐝í𝐚 𝐪𝐮𝐞 𝐩𝐚𝐬𝐚. 𝐄𝐫𝐞𝐬 𝐦𝐢 𝐢𝐧𝐬𝐩𝐢𝐫𝐚𝐜𝐢ó𝐧, 𝐦𝐢 𝐫𝐞𝐟𝐮𝐠𝐢𝐨, 𝐦𝐢 𝐭𝐨𝐝𝐨. ¿𝐓𝐞 𝐠𝐮𝐬𝐭𝐚𝐫í𝐚 𝐡𝐚𝐜𝐞𝐫𝐦𝐞 𝐞𝐥 𝐡𝐨𝐧𝐨𝐫 𝐝𝐞 𝐬𝐞𝐫 𝐦𝐢 𝐩𝐚𝐫𝐞𝐣𝐚 𝐲 𝐜𝐚𝐦𝐢𝐧𝐚𝐫 𝐣𝐮𝐧𝐭𝐨𝐬 𝐩𝐨𝐫 𝐞𝐬𝐭𝐞 𝐡𝐞𝐫𝐦𝐨𝐬𝐨 𝐯𝐢𝐚𝐣𝐞 𝐥𝐥𝐚𝐦𝐚𝐝𝐨 𝐯𝐢𝐝𝐚?",
+"\`💌𝐌𝐒𝐉 𝐃𝐄 𝐀𝐌𝐎𝐑:\`\n𝐃𝐞𝐬𝐝𝐞 𝐪𝐮𝐞 𝐞𝐧𝐭𝐫𝐚𝐬𝐭𝐞 𝐞𝐧 𝐦𝐢 𝐯𝐢𝐝𝐚, 𝐭𝐨𝐝𝐨 𝐡𝐚 𝐜𝐚𝐦𝐛𝐢𝐚𝐝𝐨 𝐝𝐞 𝐮𝐧𝐚 𝐦𝐚𝐧𝐞𝐫𝐚 𝐪𝐮𝐞 𝐧𝐮𝐧𝐜𝐚 𝐢𝐦𝐚𝐠𝐢𝐧𝐞. 𝐂𝐚𝐝𝐚 𝐜𝐨𝐧𝐯𝐞𝐫𝐬𝐚𝐜𝐢ó𝐧, 𝐜𝐚𝐝𝐚 𝐦𝐨𝐦𝐞𝐧𝐭𝐨 𝐜𝐨𝐦𝐩𝐚𝐫𝐭𝐢𝐝𝐨, 𝐡𝐚 𝐬𝐢𝐝𝐨 𝐮𝐧 𝐩𝐚𝐬𝐨 𝐦𝐚𝐬 𝐡𝐚𝐜𝐢𝐚 𝐚𝐥𝐠𝐨 𝐩𝐫𝐨𝐟𝐮𝐧𝐝𝐨 𝐲 𝐬𝐢𝐠𝐧𝐢𝐟𝐢𝐜𝐚𝐭𝐢𝐯𝐨.\n\n𝐇𝐨𝐲 𝐪𝐮𝐢𝐞𝐫𝐨 𝐬𝐞𝐫 𝐜𝐨𝐦𝐩𝐥𝐞𝐭𝐚𝐦𝐞𝐧𝐭𝐞 𝐡𝐨𝐧𝐞𝐬𝐭𝐨/𝐚 𝐜𝐨𝐧𝐭𝐢𝐠𝐨. 𝐋𝐨 𝐪𝐮𝐞 𝐬𝐢𝐞𝐧𝐭𝐨 𝐩𝐨𝐫 𝐭𝐢 𝐞𝐬 𝐦á𝐬 𝐪𝐮𝐞 𝐚𝐝𝐦𝐢𝐫𝐚𝐜𝐢ó𝐧; 𝐞𝐬 𝐮𝐧𝐚 𝐜𝐨𝐧𝐞𝐱𝐢ó𝐧 𝐪𝐮𝐞 𝐯𝐚 𝐦á𝐬 𝐚𝐥𝐥á 𝐝𝐞 𝐥𝐨 𝐪𝐮𝐞 𝐥𝐚𝐬 𝐩𝐚𝐥𝐚𝐛𝐫𝐚𝐬 𝐩𝐮𝐞𝐝𝐞𝐧 𝐞𝐱𝐩𝐫𝐞𝐬𝐚𝐫. 𝐄𝐬𝐭𝐨𝐲 𝐝𝐢𝐬𝐩𝐮𝐞𝐬𝐭𝐨/𝐚 𝐚 𝐜𝐨𝐦𝐩𝐫𝐨𝐦𝐞𝐭𝐞𝐫𝐦𝐞 𝐲 𝐚 𝐜𝐨𝐧𝐬𝐭𝐫𝐮𝐢𝐫 𝐚𝐥𝐠𝐨 𝐫𝐞𝐚𝐥 𝐲 𝐝𝐮𝐫𝐚𝐝𝐞𝐫𝐨 𝐜𝐨𝐧𝐭𝐢𝐠𝐨.\n\n𝐌𝐞 𝐞𝐧𝐜𝐚𝐧𝐭𝐚𝐫í𝐚 𝐬𝐚𝐛𝐞𝐫 𝐬𝐢 𝐬𝐢𝐞𝐧𝐭𝐞𝐬 𝐥𝐨 𝐦𝐢𝐬𝐦𝐨. ¿𝐓𝐞 𝐠𝐮𝐬𝐭𝐚𝐫í𝐚 𝐬𝐞𝐫 𝐦𝐢 𝐩𝐚𝐫𝐞𝐣𝐚 𝐲 𝐞𝐱𝐩𝐥𝐨𝐫𝐚𝐫 𝐣𝐮𝐧𝐭𝐨𝐬 𝐭𝐨𝐝𝐨 𝐥𝐨 𝐪𝐮𝐞 𝐞𝐥 𝐟𝐮𝐭𝐮𝐫𝐨 𝐭𝐢𝐞𝐧𝐞 𝐫𝐞𝐬𝐞𝐫𝐯𝐚𝐝𝐨 𝐩𝐚𝐫𝐚 𝐧𝐨𝐬𝐨𝐭𝐫𝐨𝐬? 𝐍𝐨 𝐡𝐚𝐲 𝐧𝐚𝐝𝐚 𝐪𝐮𝐞 𝐝𝐞𝐬𝐞𝐞 𝐦á𝐬 𝐪𝐮𝐞 𝐜𝐨𝐦𝐩𝐚𝐫𝐭𝐢𝐫 𝐦𝐢 𝐯𝐢𝐝𝐚 𝐜𝐨𝐧𝐭𝐢𝐠𝐨."
 ]
