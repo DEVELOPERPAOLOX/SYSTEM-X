ed9ad09e8f820e0a2de25038d924981b0be969ca
olagente38 <benjaminmijahuanga@gmail.com>
2024-08-28 04:17:48 -1100

    Update random-meme2.js

--- plugins/random-meme2.js
+++ plugins/random-meme2.js
@@ -1,8 +1,7 @@
 import hispamemes from 'hispamemes'
 let handler = async (m, { conn, usedPrefix, command }) => {
 const meme = hispamemes.meme()
-conn.sendFile(m.chat, meme, 'error.jpg', `😂😂🤣`, m)
-//await conn.sendButton(m.chat, `*_${command}_*`.trim(), wm, meme, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
+conn.sendFile(m.chat, meme, 'error.jpg', `😂😂🤣`, m, fake,)
 }
 handler.command = ['meme2', 'memes2'] 
 handler.level = 2
