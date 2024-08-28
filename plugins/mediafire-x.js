a134934eaf90653af6b3ce641c398bf5b4fc68e3
olagente38 <benjaminmijahuanga@gmail.com>
2024-08-28 04:36:13 -1100

    Update and rename descargas-mediafire.js to mediafire-x.js

--- plugins/mediafire-x.js
+++ plugins/mediafire-x.js
@@ -1,3 +1,5 @@
+// Ｃ Ｏ Ｄ Ｉ Ｇ Ｏ   Ａ Ｄ Ａ Ｐ Ｔ Ａ Ｄ Ｏ   Ｐ Ｏ Ｒ   Ｄ Ｅ Ｖ Ｅ Ｌ Ｏ Ｐ Ｅ Ｒ   Ｐ Ａ Ｏ Ｌ Ｏ   Ｘ
+
 import { mediafiredl } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
