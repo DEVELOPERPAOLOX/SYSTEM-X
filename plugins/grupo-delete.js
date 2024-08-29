d0e9b5dbc7871361d70fddadc64749ab8ac7c967
olagente38 <benjaminmijahuanga@gmail.com>
2024-08-29 08:33:29 -1100

    Update grupo-delete.js

--- plugins/grupo-delete.js
+++ plugins/grupo-delete.js
@@ -1,14 +1,26 @@
 // Ｃ Ｏ Ｄ Ｉ Ｇ Ｏ   Ａ Ｄ Ａ Ｐ Ｔ Ａ Ｄ Ｏ   Ｐ Ｏ Ｒ   Ｄ Ｅ Ｖ Ｅ Ｌ Ｏ Ｐ Ｅ Ｒ   Ｐ Ａ Ｏ Ｌ Ｏ   Ｘ
 
 const procesarEliminacion = async (mensaje, { conexion, prefijo, comando }) => {
-  if (!mensaje.quoted) throw `👋🏻𝐒𝐚𝐥𝐮𝐝𝐨𝐬 𝐮𝐬𝐮𝐚𝐫𝐢𝐨!\n🚀𝐕𝐮𝐞𝐥𝐯𝐞 𝐚 𝐮𝐬𝐚𝐫 𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐩𝐞𝐫𝐨 𝐜𝐨𝐧 𝐞𝐥 𝐮𝐬𝐨 𝐜𝐨𝐫𝐫𝐞𝐜𝐭𝐨.\n✅𝐄𝐣𝐞𝐦𝐩𝐥𝐨: .𝐝𝐞𝐥 + 𝐫𝐞𝐬𝐩𝐨𝐧𝐝𝐞 𝐚𝐥 𝐦𝐬𝐣`;
+  if (!mensaje.quoted) {
+    throw `👋🏻 ¡Hola usuario!\n🚀 Asegúrate de utilizar el comando correctamente.\n✅ Ejemplo: ${prefijo + comando} y responde al mensaje`;
+  }
 
   try {
+    if (!conexion || typeof conexion.sendMessage !== 'function') {
+      throw new Error('El objeto "conexion" no está definido correctamente o no tiene el método "sendMessage".');
+    }
+    
     const participante = mensaje.message.extendedTextMessage.contextInfo.participant;
     const mensajeId = mensaje.message.extendedTextMessage.contextInfo.stanzaId;
-    return conexion.sendMessage(mensaje.chat, { delete: { remoteJid: mensaje.chat, fromMe: false, id: mensajeId, participant: participante }});
+    
+    await conexion.sendMessage(mensaje.chat, { delete: { remoteJid: mensaje.chat, fromMe: false, id: mensajeId, participant: participante }});
   } catch (error) {
-    return conexion.sendMessage(mensaje.chat, { delete: mensaje.quoted.vM.key });
+    console.error('Error en el proceso de eliminación:', error);
+    if (mensaje.quoted && mensaje.quoted.vM && mensaje.quoted.vM.key) {
+      await conexion.sendMessage(mensaje.chat, { delete: mensaje.quoted.vM.key });
+    } else {
+      throw new Error('No se pudo eliminar el mensaje porque no se encontró la clave del mensaje citado.');
+    }
   }
 };
 
