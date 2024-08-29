44b94279387098ec36fe39ad2498f651b11ed369
olagente38 <benjaminmijahuanga@gmail.com>
2024-08-29 07:47:37 -1100

    Update Descargar-apk.js

--- plugins/Descargar-apk.js
+++ plugins/Descargar-apk.js
@@ -1,16 +1,31 @@
-import { search, download } from 'aptoide-scraper'
+import { search, download } from 'aptoide-scraper';
+
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-if (!text) throw `*Formato incorrecto*\nEjemplo:\n${usedPrefix + command} Pokemon Go`
-try {
-const searchResult = await search(text)
-const data = await download(searchResult[0].id)
-if (data.size.includes('GB') || parseFloat(data.size.replace(' MB', '')) > 300) {
-return await conn.sendMessage(m.chat, { text: '*El archivo es demasiado pesado por lo que no se enviará.*' }, { quoted: m })
-}
-await conn.sendMessage(m.chat, { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null }, { quoted: m })
-} catch {
-}}
-handler.command = /^(apkmod|modapk|dapk2|aptoide|apk)$/i
-handler.register = true
-handler.limit = 1
-export default handler
+  if (!text) throw `*Formato incorrecto*\nEjemplo:\n${usedPrefix + command} Pokemon Go`;
+  
+  try {
+    const searchResult = await search(text);
+    const data = await download(searchResult[0].id);
+    
+    if (data.size.includes('GB') || parseFloat(data.size.replace(' MB', '')) > 300) {
+      return await conn.sendMessage(m.chat, { text: '*El archivo es demasiado pesado por lo que no se enviará.*' }, { quoted: m });
+    }
+    
+    await conn.sendMessage(m.chat, {
+      document: { url: data.dllink },
+      mimetype: 'application/vnd.android.package-archive',
+      fileName: data.name + '.apk',
+      caption: null
+    }, { quoted: m });
+    
+  } catch (error) {
+    console.error(error);
+    await conn.sendMessage(m.chat, { text: '*Hubo un error al procesar la solicitud.*' }, { quoted: m });
+  }
+};
+
+handler.command = /^(apkmod|modapk|dapk2|aptoide|apk)$/i;
+handler.register = true;
+handler.limit = 1;
+
+export default handler;
