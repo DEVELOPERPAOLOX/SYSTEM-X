0bf0c924194e1e66cbaea84054501cb53b5e2ecb
olagente38 <benjaminmijahuanga@gmail.com>
2024-08-30 20:47:36 -1100

    Update main.js

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ loadChatgptDB();
 
 /* ------------------------------------------------*/
 
-global.authFile = `ShanaSession`
+global.authFile = `SystemX-Session`
 const {state, saveState, saveCreds} = await useMultiFileAuthState(global.authFile)
 const msgRetryCounterMap = (MessageRetryMap) => { }
 const msgRetryCounterCache = new NodeCache()
